cdf87358bd753ada3deeb86be57328e333ccaedd
Raymoun17 <raymoun17@gmail.com>
2020-06-17 09:45:42 -0400

    commit'

--- Firestorm/main.cpp
+++ Firestorm/main.cpp
@@ -15,10 +15,11 @@ void processInput(GLFWwindow *window)
 
 int main()
 {
+    float red = 0.2f;
+    float green = 0.2f;
+    float blue = 0.2f;
+    float alpha = 0.0f;
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow *window = glfwCreateWindow(800, 600, "Firestorm", NULL, NULL);
 
     if (window == NULL)
@@ -37,10 +38,24 @@ int main()
         return -1;
     }
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 3, (const void*)12);
+
     while (!glfwWindowShouldClose(window))
     {
         //  check for input
         processInput(window);
+        //  rendering
+        glClearColor(red, green, blue, alpha);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         //  check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
