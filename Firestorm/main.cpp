6b4923eb0e848b69b7e6ab43c0ff2dfe5ea705aa
Raymoun17 <raymoun17@gmail.com>
2020-06-18 06:30:03 -0400

    triangle

--- Firestorm/main.cpp
+++ Firestorm/main.cpp
@@ -2,11 +2,6 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -30,7 +25,6 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -38,15 +32,18 @@ int main()
         return -1;
     }
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
+    float vertices[] =
+        {
+            -0.5f, 0.5f,
+            0.5f, 0.5f,
+            0.5f, -0.5f};
+
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 3, (const void*)12);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void *)0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -55,6 +52,8 @@ int main()
         //  rendering
         glClearColor(red, green, blue, alpha);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         //  check and call events and swap the buffers
         glfwSwapBuffers(window);
